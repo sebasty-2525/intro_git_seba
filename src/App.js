21d0434cb963a8bde8bdf1514dbea126c1d66c6f
sebasty-2525 <keigomaron@gmail.com>
2025-03-21 06:37:31 +0900

    sample

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>Hello from React!</h1>
+      <p>This is a sample page deployed on Netlify.</p>
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
     </div>
   );
 }
